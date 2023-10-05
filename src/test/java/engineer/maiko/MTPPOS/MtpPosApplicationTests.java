263da20ca74e16150af692b5893d358b1614dfbf
Maiko Duarte <mkdu185@gmail.com>
2023-10-05 11:42:42 -0300

    Initial commit

---
+++ src/test/java/engineer/maiko/MTPPOS/MtpPosApplicationTests.java
@@ -0,0 +1,13 @@
+package engineer.maiko.MTPPOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtpPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
